714a0f0382a590c37a764a3cd9a405c08b106001
Pablo León <pabloleonweb-jpg@users.noreply.github.com>
2026-08-17 13:16:45 +0200

    Evitar minificado CSS incompatible en Pages

--- vite.github.config.ts
+++ vite.github.config.ts
@@ -4,5 +4,5 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-  build: { outDir: "dist", emptyOutDir: true },
+  build: { outDir: "dist", emptyOutDir: true, cssMinify: false },
 });
